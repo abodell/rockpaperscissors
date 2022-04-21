b6743eebf52b13903248fc83cab9ca048b37eca9
abodell <andybodell9@gmail.com>
2022-04-21 14:24:17 -0400

    basic rock paper scissors in the console, will update later

---
+++ game.js
@@ -0,0 +1,41 @@
+const choices = ['rock', 'paper', 'scissors']
+
+function computerPlay(picks) {
+    let choice = picks[Math.floor(Math.random() * choices.length)]
+    return choice
+}
+
+function playRound(userChoice, computerChoice) {
+    if (userChoice === 'rock' && computerChoice === 'paper') {
+        return "Computer wins! Paper beats rock!"
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return "User wins! Rock beats scissors!"
+    } else if (userChoice == 'rock' && computerChoice === 'rock') {
+        return "Tie! Rock ties Rock"
+    } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+        return "Computer wins! Scissors beats paper!"
+    } else if (userChoice === "paper" && computerChoice === 'rock') {
+        return "User wins! Paper beats rock!"
+    } else if (userChoice === 'paper' && computerChoice === 'paper') {
+        return "Tie! Paper ties Paper"
+    } else if (userChoice === 'scissors' && computerChoice === 'scissors') {
+        return "Tie! Scissors ties Scissors"
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        return "User wins! Scissors beats paper!"
+    } else {
+        return "Computer wins! Rock beats scissors!"
+    }
+}
+
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let userChoice = prompt("Pick rock, paper, scissors")
+        userChoice = userChoice.toLowerCase()
+        let computerChoice = computerPlay(choices)
+        console.log(playRound(userChoice, computerChoice))
+    }
+}
+
+game()
