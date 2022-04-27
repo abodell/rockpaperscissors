3f714c27659e2c5325e1a9632ebbf49fea55577e
abodell <andybodell9@gmail.com>
2022-04-26 20:25:07 -0400

    Finished everything except the button

--- game.js
+++ game.js
@@ -1,19 +1,7 @@
-const button = document.querySelector('.toggle');
-const theme = document.querySelector('#theme-link');
-button.addEventListener('click', function() {
-    if (theme.getAttribute('href') === "styles/dark.css") {
-        theme.href = "styles/light.css";
-        button.textContent = "Dark Mode";
-    } else {
-        theme.href = "styles/dark.css";
-        button.textContent = "Light Mode";
-    }
-})
+const choices = ['rock', 'paper', 'scissors']
 
-/*const choices = ['rock', 'paper', 'scissors']
-
-function computerPlay(picks) {
-    let choice = picks[Math.floor(Math.random() * choices.length)]
+function computerPlay(choices) {
+    let choice = choices[Math.floor(Math.random() * choices.length)]
     return choice
 }
 
@@ -39,17 +27,72 @@ function playRound(userChoice, computerChoice) {
     }
 }
 
-
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let userChoice = prompt("Pick rock, paper, scissors")
-        userChoice = userChoice.toLowerCase()
-        let computerChoice = computerPlay(choices)
-        console.log(playRound(userChoice, computerChoice))
+const button = document.querySelector('#toggle');
+const theme = document.querySelector('#theme-link');
+button.addEventListener('click', function() {
+    if (theme.getAttribute('href') === "styles/dark.css") {
+        theme.href = "styles/light.css";
+        button.textContent = "Dark Mode";
+    } else {
+        theme.href = "styles/dark.css";
+        button.textContent = "Light Mode";
     }
-}
+});
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
+// add event listeners to these buttons and then play the game with the choice
+let computerWins = 0;
+let userWins = 0;
+rock.addEventListener('click', () => {
+    let userChoice = "rock";
+    let computerChoice = computerPlay(choices);
+    let text = playRound(userChoice, computerChoice);
+    if (text.includes("Computer")) {
+        computerWins += 1;
+    } else if (text.includes("User")) {
+        userWins += 1;
+    }
+    const result = document.querySelector('#result');
+    result.textContent = text;
+    const num1 = document.querySelector(".user h1");
+    num1.textContent = userWins;
+    const num2 = document.querySelector(".computer h1");
+    num2.textContent = computerWins;
+});
 
-game() */
+paper.addEventListener('click', () => {
+    let userChoice = "paper";
+    let computerChoice = computerPlay(choices);
+    let text = playRound(userChoice, computerChoice);
+    if (text.includes("Computer")) {
+        computerWins += 1;
+    } else if (text.includes("User")) {
+        userWins += 1;
+    }
+    const result = document.querySelector('#result');
+    result.textContent = text;
+    const num1 = document.querySelector(".user h1");
+    num1.textContent = userWins;
+    const num2 = document.querySelector(".computer h1");
+    num2.textContent = computerWins;
+});
+
+scissors.addEventListener('click', () => {
+    let userChoice = "scissors";
+    let computerChoice = computerPlay(choices);
+    let text = playRound(userChoice, computerChoice);
+    if (text.includes("Computer")) {
+        computerWins += 1;
+    } else if (text.includes("User")) {
+        userWins += 1;
+    }
+    const result = document.querySelector('#result');
+    result.textContent = text;
+    const num1 = document.querySelector(".user h1");
+    num1.textContent = userWins;
+    const num2 = document.querySelector(".computer h1");
+    num2.textContent = computerWins;
+});
